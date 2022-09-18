f18262c23c0e76acce01963c77c5c59bf2505f15
Maksym Morenko <morenko.maxim@gmail.com>
2022-09-18 22:37:26 +0300

    Библиотека ract-toastify, всплывающие подсказки

--- src/features/Todos/TodoList.jsx
+++ src/features/Todos/TodoList.jsx
@@ -1,5 +1,8 @@
+import { unwrapResult } from "@reduxjs/toolkit";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 import {selectVisibleTodos, toggleTodo, removeTodo, loadTodos} from './todos-slice';
 
@@ -9,13 +12,39 @@ export const TodoList = () => {
   const todos = useSelector(state => selectVisibleTodos(state, activeFilter));
   const dispatch = useDispatch();
 
+  // реализация прелоудера и мониторинг ошибок
+  const {error, loading} = useSelector(state => state.todos)
+
   useEffect(() => {
-    dispatch(loadTodos());
+    dispatch(loadTodos())
+    // вызов метода unwrap позволит использовать и базовый зен и ловить кетчем ошибки
+    // вместо unwrap можно использовать unwrapResult из toolkit
+    .then(unwrapResult)
+    //.unwrap()
+    // по окончанию загрузки всех туду выведем уведомление (используем беблиотеку react-toastify)
+    .then(() => {
+      toast('Все туду были загружены All todo were fetch') // на загрузку поставили для примера, лучше ставить на добавление туду
+    }) // если не использовать unwrap то не зависимо от того будет ошибка или нет, мы попадаем в этот зен 
+    .catch(() => {
+      toast('ERRRRRORRRRR')
+    });
   }, [dispatch])
 
   return (
+    <>
+    <ToastContainer 
+      position="top-center"
+      autoClose={3000}
+      theme="dark" 
+    />
     <ul>
-      {todos.map((todo) => (
+      {/* добавим условия*/}
+      {/* если есть ошибка то отрисуем сообщение */}
+      {error && <h2>Error!</h2>}
+      {/* если есть загрузка т.е лоадин то отрисуем сообщение о загрузке, или паказываем прилоадер */}
+      {loading === 'loading' && <h2>loading...</h2>}
+      {/* если нет загрузки т.е айдл и если нет ошибки тогда отрисовуем туду */}
+      {loading === 'idle' && !error && todos.map((todo) => (
         <li key={todo.id}>
           <input
             type="checkbox"
@@ -27,5 +56,6 @@ export const TodoList = () => {
         </li>
       ))}
     </ul>
+    </>
   );
 };
